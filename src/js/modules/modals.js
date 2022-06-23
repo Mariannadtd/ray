e3455a87007a258951b1c1da37a23420a3505491
Ваше Имя <you@example.com>
2022-06-24 00:25:10 +0300

    hamb-modal?

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,5 +1,68 @@
 const modals = () => {
-  console.log(8);
+  function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+    const trigger = document.querySelectorAll(triggerSelector),
+          modal = document.querySelector(modalSelector),
+          close = document.querySelector(closeSelector),
+          windows = document.querySelectorAll('[data-modal]'),
+          scroll = calcScroll();
+
+    trigger.forEach(item => {
+      item.addEventListener('click', (e) => {
+        if (e.target) {
+          e.preventDefault();
+        }
+
+        windows.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        modal.style.display = "block";
+        document.body.style.overflow = "hidden";
+        document.body.style.marginRight = `${scroll}px`;
+      });
+    });
+
+    close.addEventListener('click', () => {
+      windows.forEach(item => {
+        item.style.display = 'none';
+      });
+
+      modal.style.display = "none";
+      document.body.style.overflow = "";
+      document.body.style.marginRight = `0px`;
+    });
+
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal && closeClickOverlay) {
+        windows.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        modal.style.display = "none";
+        document.body.style.overflow = ""; 
+        document.body.style.marginRight = `0px`;
+        // document.body.classList.remove('modal-open');
+      }
+    });
+  }
+
+  function calcScroll() {
+    let div = document.createElement('div');
+
+    div.style.width = '50px';
+    div.style.height = '50px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidden';
+
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+
+    return scrollWidth;
+  }
+
+  bindModal('.popup-book__btn', '.popup-book', '.popup-book .popup_close');
+  bindModal('.popup-connect__btn', '.popup-connect', '.popup-connect .popup_close');
 };
 
 export default modals;
